768bd7c9cec7d0e8c71c1971a159c7fab011b572
Jason Cassidy <47318351+jcassidyav@users.noreply.github.com>
2024-04-03 20:32:56 +0100

    bump to rc and add tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
-
+import { NativeScriptMaterialTabsModule } from "@nativescript-community/ui-material-tabs/angular";
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, NativeScriptMaterialTabsModule],
   declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
